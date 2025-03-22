e2571eb6fa440c73d172f8e3eb4fe10ca801ce1a
juan jose suarez <jjsuarezf@ufpso.edu.co>
2025-03-22 07:58:27 -0500

    :,(

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         Scanner s = new Scanner(System.in);
 
         System.out.println("Nombre vendedor:");
---
+++ src/Ejercicio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+       Scanner scanner = new scanner (System.in);
+       int intentos =0;
+       boolean acceso= false;
+       int contraseñacorrecta =1234;
+        String frasesecreta = `"la matrix vive"`;
+        String edad =18;
+        While (intentoscontraseña<3)
+        System.out.println("ingresar edad");
+        
+
+
+    }
+}
